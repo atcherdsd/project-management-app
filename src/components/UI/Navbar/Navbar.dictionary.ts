d3e99cd10ea4ea3c0f72ef2b602d9f5830a08827
agtugchik <agtugchik13@gmail.com>
2022-11-09 23:07:22 +0300

    feat: add languages for navbar

---
+++ src/components/UI/Navbar/Navbar.dictionary.ts
@@ -0,0 +1,14 @@
+export const NavbarDictionary = {
+  EN: {
+    signinup: 'Sign in / Sign up',
+    welcome: 'Welcome',
+    main: 'Main Page',
+    edit: 'Edit Profile',
+  },
+  RU: {
+    signinup: 'Войти / Зарегестрироваться',
+    welcome: 'Добро пожаловать',
+    main: 'Главная страница',
+    edit: 'Профиль',
+  },
+};
--- src/components/UI/Navbar/Navbar.tsx
+++ src/components/UI/Navbar/Navbar.tsx
@@ -3,24 +3,27 @@ import cl from './Navbar.module.scss';
 import { NavLink } from 'react-router-dom';
 import { Paths } from '../../../helpers/routerPaths';
 import { activeClassHandler } from '../../../helpers/activeClassHandler';
+import { useAppSelector } from '../../../hooks/redux';
+import { NavbarDictionary as ND } from './Navbar.dictionary';
 
 const isActiveCheck = ({ isActive }: { isActive: boolean }) =>
   activeClassHandler(isActive, cl.link, cl.link_active);
 
 const Navbar = () => {
+  const { language } = useAppSelector((state) => state.LanguageReducer);
   return (
     <nav className={cl.container}>
       <NavLink className={isActiveCheck} to={Paths.LogInPage}>
-        Sign in/Sign up
+        {language === 'EN' ? ND.EN.signinup : ND.RU.signinup}
       </NavLink>
       <NavLink className={isActiveCheck} to={Paths.WelcomePage}>
-        Welcome
+        {language === 'EN' ? ND.EN.welcome : ND.RU.welcome}
       </NavLink>
       <NavLink className={isActiveCheck} to={Paths.MainPage}>
-        Main
+        {language === 'EN' ? ND.EN.main : ND.RU.main}
       </NavLink>
       <NavLink className={isActiveCheck} to={Paths.EditProfilePage}>
-        Edit Profile
+        {language === 'EN' ? ND.EN.edit : ND.RU.edit}
       </NavLink>
     </nav>
   );
