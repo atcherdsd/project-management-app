f4a4aaebf8903f68f4c51ebd1d252aa633c33fe2
agtugchik <agtugchik13@gmail.com>
2022-11-09 22:56:34 +0300

    feat: add base name to router

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root') as Element);
 
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </Provider>
