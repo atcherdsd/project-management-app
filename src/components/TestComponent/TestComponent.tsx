1040658f850bc4ef06e0b810f6bc34bcd847d1bb
agtugchik <agtugchik13@gmail.com>
2022-11-11 21:43:14 +0300

    feat:  delete useless components

--- src/components/TestComponent/TestComponent.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import cl from './TestComponent.module.scss';
-
-const TestComponent = () => {
-  return <div className={cl.container}>Hello, Im First Component</div>;
-};
-
-export default TestComponent;
