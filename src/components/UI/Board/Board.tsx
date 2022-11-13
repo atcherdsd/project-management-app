5ac7d27fef1e5e095338834e72fc438635aee8b4
agtugchik <agtugchik13@gmail.com>
2022-11-13 19:01:41 +0300

    feat: dynamic routes for boards

--- src/components/UI/Board/Board.tsx
+++ src/components/UI/Board/Board.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import cl from './Board.module.scss';
+import { useNavigate } from 'react-router-dom';
 
 interface IBoardProps {
   owner: string;
@@ -8,9 +9,14 @@ interface IBoardProps {
   id: string;
 }
 
-const Board: FC<IBoardProps> = ({ owner, title }) => {
+const Board: FC<IBoardProps> = ({ id, owner, title }) => {
+  const navigate = useNavigate();
+
+  const boardOnClick = () => {
+    navigate(`/main/${id}`);
+  };
   return (
-    <div className={cl.container}>
+    <div className={cl.container} onClick={boardOnClick}>
       <h3 className={cl.title}>{title}</h3>
       <h4 className={cl.subtitle}>Created by {owner}</h4>
     </div>
