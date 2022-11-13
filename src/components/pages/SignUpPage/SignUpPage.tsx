731d5b170aec9bbf7bdb21e098622fd5b5b909be
mazeltovik <mazeltovik@gmail.com>
2022-11-13 11:09:18 +0300

    feat: preparing for merge

--- src/components/pages/SignUpPage/SignUpPage.tsx
+++ src/components/pages/SignUpPage/SignUpPage.tsx
@@ -4,18 +4,13 @@ import SignUpForm from '../../RegistrationForm/SignUpForm';
 import { FormValues } from '../../../types/formTypes';
 import { useSignUpAuthQuery } from '../../../API/authCalls';
 const SignInPage = () => {
-  const [signUpData, setSignUpData] = useState<FormValues>({ login: '', name: '', password: '' });
-  const { data, isError } = useSignUpAuthQuery({ path: 'signup', patch: signUpData });
-  console.log(data, isError);
-  function handleSubmit(data: FormValues) {
-    setSignUpData(data);
-  }
-
-  return (
-    <div className={cl.container}>
-      <SignUpForm handlerSubmit={handleSubmit}></SignUpForm>
-    </div>
-  );
+  // const [signUpData, setSignUpData] = useState<FormValues>({ login: '', name: '', password: '' });
+  // const { data, isError } = useSignUpAuthQuery({ path: 'signup', patch: signUpData });
+  // console.log(data, isError);
+  // function handleSubmit(data: FormValues) {
+  //   setSignUpData(data);
+  // }
+    return <div className={cl.container}>SignUpPage</div>;
 };
 
 export default SignInPage;
