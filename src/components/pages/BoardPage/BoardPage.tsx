bb2ae12f87a4d42b366af93885c73ae25cfdf734
agtugchik <agtugchik13@gmail.com>
2022-11-14 16:23:05 +0300

    feat: add back to main button for board page

--- src/components/pages/BoardPage/BoardPage.tsx
+++ src/components/pages/BoardPage/BoardPage.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import cl from './BoardPage.module.scss';
+import { useNavigate } from 'react-router-dom';
 
 const BoardPage = () => {
-  return <div className={cl.container}>BoardPage</div>;
+  const navigate = useNavigate();
+
+  const boardOnClick = () => {
+    navigate(`/main`);
+  };
+  return (
+    <div className={cl.container}>
+      <h1>Board</h1>
+      <button className={cl.button} onClick={boardOnClick}>
+        Back to Main
+      </button>
+    </div>
+  );
 };
 
 export default BoardPage;
