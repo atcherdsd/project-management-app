ecb8376c273992850fe80f36991844cffe5da319
agtugchik <agtugchik13@gmail.com>
2022-11-15 11:25:41 +0300

    feat: add drag hook for items

--- src/components/pages/BoardPage/BoardPage.tsx
+++ src/components/pages/BoardPage/BoardPage.tsx
@@ -4,6 +4,8 @@ import { useNavigate, useLocation } from 'react-router-dom';
 import { useGetAllColumnsQuery, useCreateNewColumnMutation } from '../../../API/columnsCalls';
 import { IColumn } from '../../../types/columnType';
 import Column from '../../UI/Column/Column';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 const BoardPage = () => {
   const navigate = useNavigate();
@@ -24,19 +26,21 @@ const BoardPage = () => {
   };
 
   return (
-    <div className={cl.container}>
-      <h1 className={cl.title}>Board</h1>
-      <div className={cl.columnsContainer}>
-        {data &&
-          (data as IColumn[]).map((column) => (
-            <Column key={column._id} column={column} boardId={boardId} />
-          ))}
-        <button onClick={addColumnOnClick}>Add Column</button>
+    <DndProvider backend={HTML5Backend}>
+      <div className={cl.container}>
+        <h1 className={cl.title}>Board</h1>
+        <div className={cl.columnsContainer}>
+          {data &&
+            (data as IColumn[]).map((column) => (
+              <Column key={column._id} column={column} boardId={boardId} />
+            ))}
+          <button onClick={addColumnOnClick}>Add Column</button>
+        </div>
+        <button className={cl.button} onClick={boardOnClick}>
+          Back to Main
+        </button>
       </div>
-      <button className={cl.button} onClick={boardOnClick}>
-        Back to Main
-      </button>
-    </div>
+    </DndProvider>
   );
 };
 
---
+++ src/types/dragAndDropTypes.ts
@@ -0,0 +1,4 @@
+export enum DnDTypes {
+  Task = 'TASK',
+  Column = 'COLUMN',
+}
