25d5f79220f182eaac618f437080fe4a2d1a6384
atcherdsd <tchernak.csg@gmail.com>
2022-11-29 15:47:19 +0300

    feat: add loader icon to profile editing form

--- src/components/updateUserForm/updateUserForm.tsx
+++ src/components/updateUserForm/updateUserForm.tsx
@@ -55,133 +55,139 @@ function UpdateUserForm(props: UpdateProps) {
 
   return (
     <>
-      <form className={cl.form} onSubmit={onSubmit} ref={formRef}>
-        <div className={cl.form__avatar__wrapper}>
-          <img className={cl.form__edit__logo} src={editLogo} alt="Edit Logo"></img>
+      {isLoading && (
+        <div className={cl.icon_loader_wrapper}>
+          <div className={cl.icon_loader}></div>
         </div>
-        <p className={cl.form__description}>{T('SignUpForm.edit')}</p>
-        {isSuccess && (
-          <div className={cl.form__group}>
-            <input
-              type="text"
-              id={cl.name}
-              className={cl.form__input}
-              placeholder=" "
-              autoComplete="off"
-              defaultValue={isSuccess ? name : ''}
-              {...register('name', {
-                required: { value: true, message: `${T('SignUpForm.formRequireMsg')}` },
-                pattern: {
-                  value: /^[a-zA-Zа-я\s]+$/i,
-                  message: `${T('SignUpForm.formOnlyLetter')}`,
-                },
-                minLength: { value: 2, message: `${T('SignUpForm.formMinLegthNameMsg')}` },
-              })}
-            ></input>
-            <label htmlFor={cl.name} className={cl.form__label}>
-              {T('SignUpForm.formLabelName')}
-            </label>
-            {errors.name?.types &&
-              Object.entries(errors.name?.types).map(([type, message]) => (
-                <p key={type} className={cl.error}>
-                  {message}
-                </p>
-              ))}
+      )}
+      {!isLoading && (
+        <form className={cl.form} onSubmit={onSubmit} ref={formRef}>
+          <div className={cl.form__avatar__wrapper}>
+            <img className={cl.form__edit__logo} src={editLogo} alt="Edit Logo"></img>
           </div>
-        )}
-        {isSuccess && (
-          <div className={cl.form__login}>
-            <input
-              type="text"
-              id={cl.login}
-              className={cl.form__input}
-              placeholder=" "
-              autoComplete="off"
-              defaultValue={login}
-              {...register('login', {
-                required: { value: true, message: `${T('SignUpForm.formRequireMsg')}` },
-                pattern: {
-                  value: /^[a-zA-Z0-9]+$/i,
-                  message: `${T('SignUpForm.formLoginPatternMsg')}`,
-                },
-                minLength: { value: 5, message: `${T('SignUpForm.formMinLengthMsg')}` },
-              })}
-            ></input>
-            <label htmlFor={cl.login} className={cl.form__label}>
-              {T('SignUpForm.formLogin')}
-            </label>
-            {errors.login?.types &&
-              Object.entries(errors.login?.types).map(([type, message]) => (
-                <p key={type} className={cl.error}>
-                  {message}
-                </p>
-              ))}
-          </div>
-        )}
-        {isSuccess && (
-          <div
-            className={
-              errors.login?.types
-                ? `${cl.form__password} ${cl.form__margin__pass}`
-                : `${cl.form__password}`
-            }
-          >
-            <input
-              type="password"
-              id={cl.password}
-              className={cl.form__input}
-              placeholder=" "
-              {...register('password', {
-                required: { value: true, message: `${T('SignUpForm.formRequireMsg')}` },
-                pattern: {
-                  value: /^[\w+а-я0-9]+$/i,
-                  message: `${T('SignUpForm.formLoginPatternMsg')}`,
-                },
-                minLength: { value: 5, message: `${T('SignUpForm.formMinLengthMsg')}` },
-                validate: (password: string) => {
-                  const reg = /\d+/;
-                  if (!reg.test(password) && password.length > 0)
-                    return `${T('SignUpForm.formPasswordValidateMsg')}`;
-                },
-              })}
-            ></input>
-            <label htmlFor={cl.password} className={cl.form__label}>
-              {T('SignUpForm.formPassword')}
-            </label>
-            {errors.password?.types &&
-              Object.entries(errors.password?.types).map(([type, message]) => (
-                <p key={type} className={cl.error}>
-                  {message}
-                </p>
-              ))}
-          </div>
-        )}
-        {isSuccess && (
-          <div
-            className={
-              errors.password?.types
-                ? `${cl.form__buttons} ${cl.form__margin}`
-                : `${cl.form__buttons}`
-            }
-          >
-            {isUpdating && <Spinner></Spinner>}
-            <input
-              type="submit"
-              className={cl.form__button}
-              value={T('SignUpForm.updateBtn')}
-              disabled={isUpdating ? true : false}
-            ></input>
-            <input
-              type="button"
-              className={cl.form__deleteButton}
-              defaultValue={T('SignUpForm.deleteBtn')}
-              disabled={isLoading ? true : false}
-              onClick={handleDeleteUser}
-            ></input>
-          </div>
-        )}
-      </form>
-
+          <p className={cl.form__description}>{T('SignUpForm.edit')}</p>
+          {isSuccess && (
+            <div className={cl.form__group}>
+              <input
+                type="text"
+                id={cl.name}
+                className={cl.form__input}
+                placeholder=" "
+                autoComplete="off"
+                defaultValue={isSuccess ? name : ''}
+                {...register('name', {
+                  required: { value: true, message: `${T('SignUpForm.formRequireMsg')}` },
+                  pattern: {
+                    value: /^[a-zA-Zа-я\s]+$/i,
+                    message: `${T('SignUpForm.formOnlyLetter')}`,
+                  },
+                  minLength: { value: 2, message: `${T('SignUpForm.formMinLegthNameMsg')}` },
+                })}
+              ></input>
+              <label htmlFor={cl.name} className={cl.form__label}>
+                {T('SignUpForm.formLabelName')}
+              </label>
+              {errors.name?.types &&
+                Object.entries(errors.name?.types).map(([type, message]) => (
+                  <p key={type} className={cl.error}>
+                    {message}
+                  </p>
+                ))}
+            </div>
+          )}
+          {isSuccess && (
+            <div className={cl.form__login}>
+              <input
+                type="text"
+                id={cl.login}
+                className={cl.form__input}
+                placeholder=" "
+                autoComplete="off"
+                defaultValue={login}
+                {...register('login', {
+                  required: { value: true, message: `${T('SignUpForm.formRequireMsg')}` },
+                  pattern: {
+                    value: /^[a-zA-Z0-9]+$/i,
+                    message: `${T('SignUpForm.formLoginPatternMsg')}`,
+                  },
+                  minLength: { value: 5, message: `${T('SignUpForm.formMinLengthMsg')}` },
+                })}
+              ></input>
+              <label htmlFor={cl.login} className={cl.form__label}>
+                {T('SignUpForm.formLogin')}
+              </label>
+              {errors.login?.types &&
+                Object.entries(errors.login?.types).map(([type, message]) => (
+                  <p key={type} className={cl.error}>
+                    {message}
+                  </p>
+                ))}
+            </div>
+          )}
+          {isSuccess && (
+            <div
+              className={
+                errors.login?.types
+                  ? `${cl.form__password} ${cl.form__margin__pass}`
+                  : `${cl.form__password}`
+              }
+            >
+              <input
+                type="password"
+                id={cl.password}
+                className={cl.form__input}
+                placeholder=" "
+                {...register('password', {
+                  required: { value: true, message: `${T('SignUpForm.formRequireMsg')}` },
+                  pattern: {
+                    value: /^[\w+а-я0-9]+$/i,
+                    message: `${T('SignUpForm.formLoginPatternMsg')}`,
+                  },
+                  minLength: { value: 5, message: `${T('SignUpForm.formMinLengthMsg')}` },
+                  validate: (password: string) => {
+                    const reg = /\d+/;
+                    if (!reg.test(password) && password.length > 0)
+                      return `${T('SignUpForm.formPasswordValidateMsg')}`;
+                  },
+                })}
+              ></input>
+              <label htmlFor={cl.password} className={cl.form__label}>
+                {T('SignUpForm.formPassword')}
+              </label>
+              {errors.password?.types &&
+                Object.entries(errors.password?.types).map(([type, message]) => (
+                  <p key={type} className={cl.error}>
+                    {message}
+                  </p>
+                ))}
+            </div>
+          )}
+          {isSuccess && (
+            <div
+              className={
+                errors.password?.types
+                  ? `${cl.form__buttons} ${cl.form__margin}`
+                  : `${cl.form__buttons}`
+              }
+            >
+              {isUpdating && <Spinner></Spinner>}
+              <input
+                type="submit"
+                className={cl.form__button}
+                value={T('SignUpForm.updateBtn')}
+                disabled={isUpdating ? true : false}
+              ></input>
+              <input
+                type="button"
+                className={cl.form__deleteButton}
+                defaultValue={T('SignUpForm.deleteBtn')}
+                disabled={isLoading ? true : false}
+                onClick={handleDeleteUser}
+              ></input>
+            </div>
+          )}
+        </form>
+      )}
       {incorrectData && (
         <Modal>
           <div className={cl.modal__container}>
